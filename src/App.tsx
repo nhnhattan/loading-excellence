066e97290bd2c70c5c827d0fe87987072760d5d2
Nhật Tân <nhoxcon029@gmail.com>
2025-12-24 14:13:14 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -104,10 +104,29 @@ function App() {
                     </div>
                     <p className="text-white uppercase">nơi làm việc</p>
                     <div className="bg-input w-4/5">
-                      <input
-                        type="text"
-                        className="w-full text-center text-white focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none py-2 px-2"
-                      />
+                      <select
+                        className="w-[95%] text-sm text-center text-white uppercase font-bold
+               focus-visible:ring-0 focus-visible:ring-offset-0
+               focus-visible:outline-none py-3 px-2"
+                        defaultValue=""
+                      >
+                        <option
+                          value=""
+                          disabled
+                          className="bg-black text-white disabled:text-white"
+                        >
+                          Chọn thành phố
+                        </option>
+                        <option value="ha-noi" className="bg-red-500">
+                          Hà Nội
+                        </option>
+                        <option value="ho-chi-minh" className="bg-red-500">
+                          Hồ Chí Minh
+                        </option>
+                        <option value="da-nang" className="bg-red-500">
+                          Đà Nẵng
+                        </option>
+                      </select>
                     </div>
                   </div>
                 </div>
@@ -196,22 +215,23 @@ function App() {
                   <div className="w-2/5 mx-auto mb-12">
                     <img src="./img/logo2.png" alt="" />
                   </div>
-                  <div className="bg-frame w-4/5 pt-4 mx-auto flex flex-col items-center justify-center">
-                    <p className="text-yellow-300 font-bold">
-                      Link của tư vấn viên
-                    </p>
-                    <div className="bg-input w-[90%] mb-3">
-                      <input
-                        type="text"
-                        disabled
-                        value={"daylalink.com"}
-                        ref={inputRef}
-                        className="w-full text-center text-white focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none py-2 px-2"
-                      />
-                    </div>
-                  </div>
+
                   {person === "agency" ? (
                     <>
+                      <div className="bg-frame w-4/5 pt-4 mx-auto flex flex-col items-center justify-center">
+                        <p className="text-yellow-300 font-bold">
+                          Link của tư vấn viên
+                        </p>
+                        <div className="bg-input w-[90%] mb-3">
+                          <input
+                            type="text"
+                            disabled
+                            value={"daylalink.com"}
+                            ref={inputRef}
+                            className="w-full text-center text-white focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none py-2 px-2"
+                          />
+                        </div>
+                      </div>
                       <div className="flex flex-col items-center justify-center gap-2 mt-4">
                         <button
                           onClick={handleShareDrawer}
@@ -245,14 +265,10 @@ function App() {
                     </>
                   ) : (
                     <>
-                      {" "}
                       <div className="flex flex-col items-center justify-center gap-2 mt-4">
                         <button
                           onClick={() => {
-                            window.open(
-                              `https://docs.google.com/`,
-                              "_blank"
-                            );
+                            window.open(`https://docs.google.com/`, "_blank");
                           }}
                           className="w-3/6 relative cursor-pointer hover:opacity-85"
                         >
